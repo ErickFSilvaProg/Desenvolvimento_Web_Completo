6f7b03a6e5e87afcc762bc0d3b59d1cc90edd796
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-08-19 15:59:11 -0300

    Seção 13 - Linguagem programação PHP, 19/08/25 (tarde)

---
+++ secao-13-linguagem-programacao-php/aula309-embutindo-blocos-php-paginas-html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Aula 309</title>
+</head>
+<body>
+
+    <h1>PHP - Aula 309</h1>
+
+    <!-- Tag padrão do PHP -->
+    <?php
+        echo "Tag padrão do PHP";
+    ?>
+    <br>
+
+    <!-- Tag de impressão do PHP -->
+    <?= "Tag de impressão do PHP" ?>
+    
+</body>
+</html>
