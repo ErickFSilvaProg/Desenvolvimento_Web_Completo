594d9a61b6a9fbec8a54b4c3fc331ed034645556
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-08-20 20:27:48 -0300

    Correção, 20/08/25 (noite)

---
+++ secao-13-linguagem-programacao-php/aula310-tags-curtas-php/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Tag curta</title>
+</head>
+
+<body>
+
+<?php
+    echo "Tag padrão do PHP.";
+?>
+<br>
+
+<?=
+    "Tag de impressão do PHP";
+?>
+<br>
+
+<?
+    echo "Tag curta do PHP.";
+?>
+
+</body>
+
+</html>
