2750434fc3e9dc9678858a699842f22a9e8ce6d7
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-09-04 16:10:38 -0300

    Seção 13 - PHP, 04/09/2025 (tarde)

--- secao-13-linguagem-programacao-php/aula311-saida-de-dados-com-echo-e-print/index.php
+++ secao-13-linguagem-programacao-php/aula311-saida-de-dados-com-echo-e-print/index.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
 </head>
+
 <body>
 
-<?php
+    <p>
+        O "echo" e o "print" são janelas entre a programação e o arquivo "html".
+    </p>
 
-// O 'echo' é um construtor da liguagem.
-echo 'Comando echo<br>';
+    <?php
 
-// O 'print' é uma função que possui um retorno.
-echo print "Comando print<hr>";
+    // O 'echo' é um construtor da liguagem.
+    echo 'Comando echo<br>';
 
-?>
-    
+    // O 'print' é uma função que possui um retorno.
+    print "Comando print<br>";
+    echo print "";
+
+    ?>
+
 </body>
+
 </html>
